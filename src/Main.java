d725547db6ff9dd1d8af9bbec022faf6206b30df
Pranav Sharma <pranavsharmapranav2004@gmail.com>
2025-01-23 20:15:01 +0530

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Integer> myMap = new HashMap<>();
+
+        myMap.put("pranav",6);
+        myMap.put("kundan",7);
+        System.out.println(myMap);
+    }
+}
