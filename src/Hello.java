f1121e7bf71d6751a2a657dd3a59ac6f39c3687d
Pranav Sharma <pranavsharmapranav2004@gmail.com>
2025-01-25 20:23:37 +0530

    change

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello ,World!");
+        System.out.println("good");
     }
 }
